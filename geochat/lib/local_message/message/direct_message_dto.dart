e5e6301db214f64e1fb90aa861aba1afdba4ed38
Rafael Corbellini <rafael.corbellini@egsys.com.br>
2020-07-14 18:38:24 -0300

    adicionado mais models dto

---
+++ geochat/lib/local_message/message/direct_message_dto.dart
@@ -0,0 +1,11 @@
+import 'package:geochat/local_message/local/local_dto.dart';
+import 'package:geochat/user/user_dto.dart';
+
+class DirectMessage{
+  final String text;
+  final String title;
+  final User owner;
+  final Local local;   
+
+  DirectMessage({this.text, this.title, this.owner, this.local});
+}
