bf3e60ef311fc670c5bf69f59150dcf88379488f
Rafael Corbellini <rafael.corbellini@egsys.com.br>
2020-07-30 07:23:24 -0300

    enhanc: home + navegacao

--- geochat/lib/_shared/route/sailor_navigator.dart
+++ geochat/lib/_shared/route/sailor_navigator.dart
@@ -4,6 +4,7 @@ import 'package:sailor/sailor.dart';
 final Sailor sailor = Sailor();
 
 class SailorNavigator extends Navigator {
+
   @override
   Future<T> navigateTo<T>(String route, {Map<String, dynamic> params}) {
     return sailor.navigate<T>(route, params: params);
--- geochat/lib/home/home_di.dart
+++ geochat/lib/home/home_di.dart
@@ -5,23 +5,7 @@ import 'package:geochat/local_message/talk/talk_repository.dart';
 class HomeDi {
   Injector initialise(Injector injector) {
     injector.map<TalkRepository>((i) => TalkDataRepository());
-
     injector.map<HomeBloc>((i) => HomeBloc(talkRepository: i.get()));
-
-    /**     
-
-    injector.map<Logger>((i) => Logger(), isSingleton: true);
-    injector.map<String>((i) => "https://api.com/", key: "apiUrl");
-    injector.map<SomeService>(
-        (i) => SomeService(i.get<Logger>(), i.get<String>(key: "apiUrl")));
-
-    injector.map<SomeType>((injector) => SomeType("0"));
-    injector.map<SomeType>((injector) => SomeType("1"), key: "One");
-    injector.map<SomeType>((injector) => SomeType("2"), key: "Two");
-
-    injector.mapWithParams<SomeOtherType>((i, p) => SomeOtherType(p["id"]));
-
-     */
 
     return injector;
   }
