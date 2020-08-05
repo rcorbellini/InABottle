c96ffa0ae22677bc0752aecb3258fce2e3794da7
Rafael Corbellini <rafael.corbellini@egsys.com.br>
2020-08-05 09:14:44 -0300

    enhanc: criado repository sem implementaçao

---
+++ geochat/lib/user/user_repository.dart
@@ -0,0 +1,29 @@
+import 'package:geochat/_shared/archtecture/base_repository.dart';
+import 'package:geochat/user/user_dto.dart';
+
+class UserRepository extends BaseRepository<User, String> {
+  @override
+  Future delete() {
+    return null;
+  }
+
+  @override
+  Future<List<User>> loadAll() {
+    return null;
+  }
+
+  @override
+  Future<User> loadByKey(String key) {
+    return null;
+  }
+
+  @override
+  Future save() {
+    return null;
+  }
+
+  @override
+  Future saveAll() {
+    return null;
+  }
+}
