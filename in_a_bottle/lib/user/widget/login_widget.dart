29229a2e7616b6890823a838c6a36f764151768e
Rafael Corbellini <rafaelcorbellini@gmail.com>
2020-08-06 22:40:28 -0300

    enhanc: 1 adicionado firebase, porem nao parece ta de acordo (#14)
    Co-authored-by: Rafael Corbellini <rafael.corbellini@egsys.com.br>

--- in_a_bottle/lib/user/widget/login_widget.dart
+++ in_a_bottle/lib/user/widget/login_widget.dart
@@ -10,7 +10,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 GoogleSignIn _googleSignIn = GoogleSignIn(
   scopes: [
-    'email',
+    'email','https://www.googleapis.com/auth/contacts.readonly',    
   ],
 );
 
